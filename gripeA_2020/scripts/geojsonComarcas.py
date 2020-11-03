f3407024f43fbbb86869d4a70de529f71626ce37
Emilio <emiliova@ucm.es>
2020-11-03 20:03:42 +0100

    cuadrados comarcas
    Falta subirlo a la bbdd

--- gripeA_2020/scripts/geojsonComarcas.py
+++ gripeA_2020/scripts/geojsonComarcas.py
@@ -7,10 +7,70 @@ client= MongoClient('mongodb://localhost:27017/')
 db = client.lv
 comarca = db.comarcas
 
-#Extraccion de datos
-geojson = {}
-cursor = comarca.find({})
+#Extraccion de los cuatro rectangulos
+cursor = comarca.find()
+array = {}
+izqI = []
+izqS = []
+derI = []
+derS = []
+iterador= 0
+for it in cursor:
+    coordenadas = it['coordinates']
+    aux = 0
+   
+    for i in range(len(coordenadas)):
+        if aux == 0:
+            if len(coordenadas[i][0]) > 3: 
+                minX = coordenadas[i][0][0][0] #Inicializo los puntos cuando hay un array dentro de un array
+                maxX = coordenadas[i][0][0][0]
+                minY = coordenadas[i][0][0][1]
+                maxY = coordenadas[i][0][0][1]
+            else: 
+                minX = coordenadas[i][0][0] #Inicializo los puntos 
+                maxX = coordenadas[i][0][0]
+                minY = coordenadas[i][0][1]
+                maxY = coordenadas[i][0][1]
 
-print(cursor)
+        for j in range(len(coordenadas[i])):
+
+            if len(coordenadas[i][j]) > 3:
+                for k in range(len(coordenadas[i][j])):
+                    if coordenadas[i][j][k][0] < minX:
+                        minX = coordenadas[i][j][k][0]
+                    elif coordenadas[i][j][k][0] > maxX:
+                        maxX = coordenadas[i][j][k][0]
+                    
+                    if coordenadas[i][j][k][1] < minY:
+                        minY = coordenadas[i][j][k][1]
+                    elif coordenadas[i][j][k][1] > maxY:
+                        maxY = coordenadas[i][j][k][1] 
+            else:
+                if coordenadas[i][j][0] < minX:
+                    minX = coordenadas[i][j][0]
+                elif coordenadas[i][j][0] > maxX:
+                    maxX = coordenadas[i][j][0]
+                
+                if coordenadas[i][j][1] < minY:
+                    minY = coordenadas[i][j][1]
+                elif coordenadas[i][j][1] > maxY:
+                    maxY = coordenadas[i][j][1]
+        aux = 1
+
+    iterador+=1
+    izqI.append([minX,minY]) #guardo los puntos en las listaas
+    izqS.append([minX,maxY])
+    derI.append([maxX,minY])
+    derS.append([maxX,maxY])
+
+#se guardan las 4 listas en la base de datos
+
+
+
+
+
+        
+   
+
 #Carpeta destino
 
