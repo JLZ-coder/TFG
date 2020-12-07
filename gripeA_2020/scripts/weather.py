2e12251a2c3b27d8c67cfdca40b88faf55a676ac
Emilio <emiliova@ucm.es>
2020-12-07 16:51:30 +0100

    Acceso API 0.1

--- gripeA_2020/scripts/weather.py
+++ gripeA_2020/scripts/weather.py
@@ -2,7 +2,7 @@ import json
 import requests
 import re
 import pandas as pd
-from aemet import *
+#from aemet import *
 import pymongo
 from pymongo import MongoClient
 
@@ -13,7 +13,7 @@ from pymongo import MongoClient
 
 #Leemos el fichero que relaciona las estaciones con las comarcas
 file = "aemet/Estaciones_CG.xlsx"
-
+api_key='eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlbWlsaW92YUB1Y20uZXMiLCJqdGkiOiJiZDc2MzgzMS1hMWU4LTQ4MTktOTE2Yy1lYzQ5MjE2OTJiYjAiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTYwNjQ3NTk4NiwidXNlcklkIjoiYmQ3NjM4MzEtYTFlOC00ODE5LTkxNmMtZWM0OTIxNjkyYmIwIiwicm9sZSI6IiJ9.0Z3PqEjKyMFhUztyu2LAPV7zYPEaeh2RXndZQdryTrE'
 df = pd.read_excel(file)
 #diccionario que tendra como clave "id de comarca" y valor la informacion de las estaciones
 #Insercion de los datos en la base de datos
@@ -26,7 +26,27 @@ estacion.delete_many({})
 estacion.insert_many(records)
 
 
+#Extraer la informacion de la aemet
 
+#datosmensualesAnuales
+
+url_mensualAnual = "https://opendata.aemet.es/opendata/api/valores/climatologicos/mensualesanuales/datos/anioini/2020/aniofin/2020/estacion/0252D/?api_key={}".format(api_key)
+
+headers = {
+    'cache-control': "no-cache"
+    }
+
+#Extraemos la url donde esta la informacion de la consulta a la API
+response = requests.request("GET", url_mensualAnual, headers=headers)
+
+json_response = response.json()
+
+#Extraemos la informacion de la API
+response = requests.request("GET", json_response['datos'], headers=headers)
+
+json_response = response.json()
+
+print(json_response).pretty()
        
         
 
