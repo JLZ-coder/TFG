3fe1c4902dd7d27d5efb540a411c650779fda25c
Emilio <emiliova@ucm.es>
2021-02-22 17:59:12 +0100

    Update species_mongo.py

--- gripeA_2020/scripts/species_mongo.py
+++ gripeA_2020/scripts/species_mongo.py
@@ -3,17 +3,25 @@ import pymongo
 from pymongo import MongoClient
 
 file = "data/Datos especies1.xlsx"
-df = pd.read_excel(file, 'Especies_equivalencias')
+#df = pd.read_excel(file, 'Especies_equivalencias')
+#df1 = pd.read_excel(file, 'Prob_migracion', skiprows=3, usecols='A:AY', header=0, index_col=2 )
 df1 = pd.read_excel(file, 'Prob_migracion', skiprows=3, usecols='A:AY', header=0)
 
 #df1 = df1.drop([0,1,2],axis=0)
 
 df1 = df1.fillna("")
-print(df1)
+#forma de acceder a la prob de movimiento segun el codigo de anilla index
+#print(df1[3][70])
 
+print(df1.index)
+df_mongo = pd.DataFrame({'Nombre cientifico': df1['Nombre científico'], 'Especie': df1['Especie'], 'codigo anilla': df1['codigo anilla']})
+
+
+client= MongoClient('mongodb://localhost:27017/')
+db = client.lv
 #Especies
 especie = db.especies
-records = df_especies.to_dict(orient='records')  # Here's our added param..
+records = df_mongo.to_dict(orient='records')  # Here's our added param..
 
 # Borra todo
 especie.delete_many({})
