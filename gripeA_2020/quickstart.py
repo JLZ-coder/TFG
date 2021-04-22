d7689a004549b306e00f2fd8ca892db50b0c77d3
JLZ-coder <60971493+JLZ-coder@users.noreply.github.com>
2021-04-22 18:08:19 +0200

    comprobar pydrive

--- gripeA_2020/quickstart.py
+++ gripeA_2020/quickstart.py
@@ -1,4 +1,38 @@
 from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+
 
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+
+# Try to load saved client credentials
+gauth.LoadCredentialsFile("mycreds.txt")
+if gauth.credentials is None:
+    # Authenticate if they're not there
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    # Refresh them if expired
+    gauth.Refresh()
+else:
+    # Initialize the saved creds
+    gauth.Authorize()
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("mycreds.txt")
+
+drive = GoogleDrive(gauth)
+
+# file1 = drive.CreateFile()  # Create GoogleDriveFile instance
+# file1.SetContentFile('markdown/informePrueba.pdf') # Set content of the file from given string.
+# file1.Upload()
+
+folderName = 'informe'  # Please set the folder name.
+filepath = 'markdown/informePrueba.pdf'
+files_from_path = filepath.split('/')
+filename = files_from_path[-1]
+
+folders = drive.ListFile(
+    {'q': "title='" + folderName + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
+for folder in folders:
+    if folder['title'] == folderName:
+        file1 = drive.CreateFile({'title': filename,'parents': [{'id': folder['id']}]}) # Carpeta informe
+        file1.SetContentFile(filepath)
+        file1.Upload()
