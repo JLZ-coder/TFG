9906b1eec9829c730441e96b9743d1e1ba2628e7
JLZ-coder <60971493+JLZ-coder@users.noreply.github.com>
2021-04-25 21:19:59 +0200

    geojson update

--- gripeA_2020/controller/controller.py
+++ gripeA_2020/controller/controller.py
@@ -91,7 +91,7 @@ class Controller:
         #Semana actual
         start = date.today() + timedelta(days = -date.today().weekday())
         #Fecha hace 364 dias, 52 semanas
-        this_many_weeks = 8
+        this_many_weeks = 0
         start -= timedelta(weeks=this_many_weeks)
         end = start + timedelta(weeks = this_many_weeks)
 
@@ -187,7 +187,7 @@ class Controller:
             # -----------------------------------------------------------
 
             print(">>> Alertas total: " + str(len(alertas["alertas"])))
-            print(">>> Alertas en riesgo: " + str(len(comarcas_en_riesgo)))
+            print(">>> Alertas con rutas en cuenta: " + str(len(comarcas_en_riesgo)))
 
             # Rellenar el informe semanal
             reportPDF = self.dataFactory.createData("report",current_week, current_week_end, alertas)
@@ -201,10 +201,121 @@ class Controller:
         geojson_outbreak = self.geojsonGen.generate_outbreak(brotes_por_semana)
         geojson_migration = self.geojsonGen.generate_migration(migrations_por_semana, lista_comarcas, brotes_por_semana)
 
-        #broteEspecie = dict()
-        #broteEspecie[288337] = {"cientifico" : "Patito" ,"especie": "pollitus", "codigoE":70, "probEspecie": 0.2}
-        #alertas["alertas"].append({"comarca_sg" : "SP01059", "risk" : 3, "temperatura": 2.0, "brotes": broteEspecie })
-        #reportPDF = self.dataFactory.createData("report",start, end, alertas)
+        return 0
+
+    def runOnlineTool_1year(self):
+        #Semana actual
+        start = date.today() + timedelta(days = -date.today().weekday())
+        #Fecha hace 364 dias, 52 semanas
+        this_many_weeks = 52
+        start -= timedelta(weeks=this_many_weeks)
+        end = start + timedelta(weeks = this_many_weeks)
+
+        #Convert to datetime
+        start = datetime.combine(start, datetime.min.time())
+        end = datetime.combine(end, datetime.min.time())
+
+        #DATA SENT TO MODEL
+
+        #Outbreaks
+        # brotes_por_semana
+        # 2020/01/01 => list(brotes en esta semana)
+        # 2020/01/08 => list(brotes en esta semana)
+        # ...
+        # ..
+        # .
+        # comarca_brotes
+        # *Codigo de una comarca* => list(info de brotes que llegan a esta comarca)
+        # ...
+        # ..
+        # .
+
+        # 12 semanas = 84 dias = aprox. 3 meses
+        # outbreakStart = start - timedelta(weeks = 12)
+        # comarca_brotes, brotes_por_semana = self.dataFactory.createData("outbreak", outbreakStart, end , None)
+
+        #Temperature
+        #tMin = self.dataFactory.createData("temp",start, end, True)
+
+        #Comarca
+        lista_comarcas = self.dataFactory.createData("comarcas", None, None, None)
+
+        #Probabilidad Migracion
+        file = "data/Datos especies1.xlsx"
+        matrizEspecies = pd.read_excel(file, 'Prob_migracion', skiprows=3, usecols='A:AY', header=0, index_col=2)
+
+        #DATA SENT TO MODEL
+        data_to_model= dict()
+        # data_to_model['comarca_brotes']= comarca_brotes
+        # data_to_model['tMin'] = tMin
+        data_to_model['matrizEspecies'] = matrizEspecies
+        data_to_model['online'] = True
+
+        #Geojson generator
+        migrations_por_semana = dict()
+        brotes_por_semana = dict()
+        alertas_list = list()
+
+        comarcas_en_riesgo = set()
+
+        i = 0
+        current_week = start
+        current_week_end = current_week + timedelta(weeks=1)
+        while (i <= this_many_weeks):
+            print("Run model para semana " + str(current_week))
+            # 12 semanas = 84 dias = aprox. 3 meses
+            outbreakStart = current_week - timedelta(weeks = 12)
+            # brotes_por_semana_aux no se usa
+            comarca_brotes, brotes_esta_semana = self.dataFactory.createData("outbreak", outbreakStart, current_week_end , None)
+            brotes_por_semana[current_week] = brotes_esta_semana
+
+            #Temperature
+            tMin = self.dataFactory.createData("temp",current_week, current_week_end, True)
+
+            #DATA SENT TO MODEL
+            data_to_model['comarca_brotes']= comarca_brotes
+            data_to_model['tMin'] = tMin
+
+            # RUN MODEL
+            self.model.setData(data_to_model)
+            alertas = self.model.run(current_week, current_week_end)
+
+            # Quitamos las alertas de riesgo 0
+            alertas["alertas"] = list(filter(lambda alerta: alerta["risk"] != 0, alertas["alertas"]))
+
+            alertas_list.append(alertas)
+
+            # Para seleccionar solo las rutas de comarcas en riesgo > 0
+            # -----------------------------------------------------------
+            comarcas_en_riesgo.clear()
+            for alerta in alertas["alertas"]:
+                if current_week not in migrations_por_semana:
+                    migrations_por_semana[current_week] = {}
+
+                migrations_por_semana[current_week][alerta["comarca_sg"]] = comarca_brotes[alerta["comarca_sg"]]
+
+            # for comarca in comarca_brotes:
+            #     if comarca in comarcas_en_riesgo:
+            #         if current_week not in migrations_por_semana:
+            #             migrations_por_semana[current_week] = {}
+
+            #         migrations_por_semana[current_week][comarca] = comarca_brotes[comarca]
+            # -----------------------------------------------------------
+
+            print(">>> Alertas total: " + str(len(alertas["alertas"])))
+            print(">>> Alertas con rutas en cuenta: " + str(len(comarcas_en_riesgo)))
+
+            # Rellenar el informe semanal
+            reportPDF = self.dataFactory.createData("report",current_week, current_week_end, alertas)
+
+            #Actualizar current_week y current_week_end
+            current_week = current_week_end
+            current_week_end = current_week + timedelta(weeks = 1)
+            i += 1
+
+        geojson_alerta = self.geojsonGen.generate_alerta(alertas_list, lista_comarcas)
+        geojson_outbreak = self.geojsonGen.generate_outbreak(brotes_por_semana)
+        geojson_migration = self.geojsonGen.generate_migration(migrations_por_semana, lista_comarcas, brotes_por_semana)
 
         return 0
 
--- gripeA_2020/model/GeojsonGenerator.py
+++ gripeA_2020/model/GeojsonGenerator.py
@@ -1,7 +1,11 @@
 import math
 from datetime import datetime, timedelta, date
 import json
+<<<<<<< Updated upstream
 from model.gdriveUploader import gDriveUploader
+=======
+import os
+>>>>>>> Stashed changes
 
 class GeojsonGenerator:
     def __init__(self):
@@ -56,6 +60,67 @@ class GeojsonGenerator:
               
         text_file = open("geojson/alertas.geojson", "w", encoding="utf-8")
         n = text_file.write(json.dumps(feat_col_alertas, ensure_ascii=False))
+        text_file.close()
+
+        return feat_col_alertas
+
+    def update_alerta(self, alertList, comarcasDict):
+        feat_col_alertas = {
+            "type": "FeatureCollection",
+            "features": []
+        }
+
+        f = open("geojson/alertas.geojson")
+        json_alertas = json.load(f)
+
+        ultima_fecha = datetime.min
+        for alerta in json_alertas["features"]:
+            if alerta["properties"]["reportDate"] > ultima_fecha:
+                ultima_fecha = alerta["properties"]["reportDate"]
+
+
+        comarcas_de_alertlist = set()
+        it = dict()
+
+        for alertas in alertList:
+            start = alertas["start"]
+            if start > ultima_fecha:
+                end = alertas["end"]
+                start.replace(hour=1)
+                end.replace(hour=1)
+                comarcas_de_alertlist.clear()
+
+                for it in alertas["alertas"]:
+                    if it['risk'] != 0:
+                        comarcas_de_alertlist.add(it['comarca_sg'])
+                        cod_comarca = it['comarca_sg']
+                        it['Longitud'] = comarcasDict[cod_comarca]['Longitud']
+                        it['Latitud'] = comarcasDict[cod_comarca]['Latitud']
+                        it['com_sgsa_n'] = comarcasDict[cod_comarca]['com_sgsa_n']
+                        it['CPRO'] = comarcasDict[cod_comarca]['CPRO']
+                        it['provincia'] = comarcasDict[cod_comarca]['provincia']
+                        it['CPROyMUN'] = comarcasDict[cod_comarca]['CPROyMUN']
+
+                        aux={
+                            "type": "Feature",
+                            "geometry": {
+                                "type": "Point",
+                                "coordinates": [float(it['Longitud']), float(it['Latitud'])]
+                            },
+                            "properties": {
+                                "idAlerta": it['comarca_sg'] + " " + start.strftime("%d-%m-%Y"),
+                                "Riesgo": it['risk'],
+                                "reportDate": start.timestamp() * 1000,
+                                "comarca": it['com_sgsa_n'],
+                                "informe": ""
+                            }
+                        }
+                        feat_col_alertas["features"].append(aux)
+
+        json_alertas["features"].append(feat_col_alertas["features"])
+
+        text_file = open("geojson/alertas.geojson", "w", encoding="utf-8")
+        n = text_file.write(json.dumps(json_alertas, ensure_ascii=False))
         text_file.close()
 
         return feat_col_alertas
@@ -121,7 +186,107 @@ class GeojsonGenerator:
 
         return feat_col_migracion
 
+    def update_migration(self, outbreakComarca, comarcasDict, brotesDict):
+        feat_col_migracion = {
+            "type": "FeatureCollection",
+            "features": []
+        }
+
+        for semana in outbreakComarca:
+
+            migration_dict = dict()
+
+            for cod_comarca in outbreakComarca[semana]:
+
+                migration_dict[cod_comarca] = dict()
+                migration_dict[cod_comarca]["oieids"] = dict()
+                dict_oieid = migration_dict[cod_comarca]["oieids"]
+                migration_dict[cod_comarca]["lat"] = comarcasDict[cod_comarca]["Latitud"]
+                migration_dict[cod_comarca]["long"] = comarcasDict[cod_comarca]["Longitud"]
+
+                for brote in outbreakComarca[semana][cod_comarca]:
+
+                    if brote["oieid"] not in dict_oieid:
+                        dict_oieid[brote["oieid"]] = brote["especie"]
+
+
+            for cod_comarca in migration_dict:
+
+                comarca_long = migration_dict[cod_comarca]["long"]
+                comarca_lat = migration_dict[cod_comarca]["lat"]
+
+                for oieid in migration_dict[cod_comarca]["oieids"]:
+
+                    for semana in brotesDict:
+                        for brote in brotesDict[semana]:
+                            if brote["oieid"] == oieid:
+                                current_brote = brote
+                                break
+
+                    aux = {
+                            "type": "Feature",
+                            "geometry": {
+                                "type": "LineString",
+                                "coordinates": [
+                                    [float(comarca_long), float(comarca_lat)],
+                                    [float(current_brote['long']), float(current_brote['lat'])]
+                                ]
+                            },
+                            "properties": {
+                                "idBrote": oieid,
+                                "idAlerta": cod_comarca + " " + semana.strftime("%d-%m-%Y"),
+                                "idComarca": cod_comarca,
+                            }
+                        }
+
+                    feat_col_migracion['features'].append(aux)
+
+        text_file = open("geojson/rutas.geojson", "w", encoding="utf-8")
+        n = text_file.write(json.dumps(feat_col_migracion, ensure_ascii=False))
+        text_file.close()
+
+        return feat_col_migracion
+
     def generate_outbreak(self, outbreaklist):
+        feat_col_brote = {
+            "type": "FeatureCollection",
+            "features": []
+        }
+
+        for semana in outbreaklist:
+            for it in outbreaklist[semana]:
+                if ('city' not in it):
+                    it['city'] = "No especificado"
+                aux = {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [float(it['long']), float(it['lat'])]
+                        },
+                        "properties": {
+                            "id": it['oieid'],
+                            "disease": it['disease'],
+                            "country": it['country'],
+                            "observationDate": math.floor(it['observation_date'].timestamp() * 1000),
+                            "city": it['city'],
+                            "species": it['species'],
+                            "cases": "" if it['cases']== "" else int(it['cases']),
+                            "deaths": "" if it['deaths']== "" else int(it['deaths']),
+                            "serotipo": it['serotype'],
+                            "moreInfo": it['urlFR'],
+                            "epiUnit": it['epiunit'],
+                        }
+                    }
+
+                feat_col_brote['features'].append(aux)
+
+        text_file = open("geojson/brotes.geojson", "w", encoding="utf-8")
+        n = text_file.write(json.dumps(feat_col_brote, ensure_ascii=False))
+        text_file.close()
+
+        return feat_col_brote
+
+    def update_outbreak(self, outbreaklist):
         feat_col_brote = {
             "type": "FeatureCollection",
             "features": []
